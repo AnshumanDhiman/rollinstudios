e0f397ff82d8a9b77cc5684c2543e671e11ed51a
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-07-03 01:14:37 +0530

    fix1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Container from './components/Container/Container';
-import New from './components/NewCar/New';
 
 function App() {
   return (
--- src/components/Cards/Music/MCards.js
+++ src/components/Cards/Music/MCards.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from "pure-react-carousel";
-import "pure-react-carousel/dist/react-carousel.es.css";
 import i1 from '../../../assets/images/3.jpg';
 /* Install pure-react-carousel using -> npm i pure-react-carousel */
 
